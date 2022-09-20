e7d6090361f44c3e28ce8fbe8c1d3d0c465676b6
ebuckmas <evanbuckmaster@gmail.com>
2022-09-20 16:44:56 -0400

    completed remove button function

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-
-
-//logic for modal window and DOM manipulation
-
-const modal = document.querySelector('.modal');
-const trigger = document.querySelector('.trigger');
-const closeButton = document.querySelector('.close-button');
-const submitButton = document.getElementById('submitButton');
-
-
-
 //initialize library array
 
 let myLibrary = [];
@@ -21,31 +10,31 @@ function Book(title, author, pages, read) {
     this.pages = pages
     this.read = read
     // this.info = function() {
-    //     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
-    // }
+        //     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
+        // }
 }
-
+    
 Book.prototype.info = function() {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
 }
-
-//function to take in user's input to create a new book
-
+    
+//function to take in user's input to create a new book and add to myLibrary array
+    
 function addBookToLibrary(userTitle, userAuthor, userPages, userRead) {
     let userBook = new Book(userTitle, userAuthor, userPages, userRead);
     myLibrary.push(userBook);
 }
-
+    
 //initializing a few books to start the array
-
+    
 addBookToLibrary('Game of Thrones', 'George R.R. Martin', 1000, 'read');
-
+    
 addBookToLibrary('1984', 'George Orwell', 250, 'read');
 
 //function to create new cards and append to DOM
+const library = document.querySelector('.library');
 
 function createCard(title, author, pages, read, arrayIndex) {
-    const library = document.querySelector('.library');
     const card = document.createElement('div');
     card.classList.add('card');
     card.setAttribute('id', arrayIndex);
@@ -65,12 +54,12 @@ function createCard(title, author, pages, read, arrayIndex) {
     const bookRead = document.createElement('p');
     bookRead.textContent = read;
     card.appendChild(bookRead);
-
+    
     const closeButton = document.createElement('button');
     closeButton.setAttribute('class', 'remove');
     closeButton.textContent = 'Remove';
     card.appendChild(closeButton);
-
+    
     const index = document.createElement('p');
     index.textContent = arrayIndex;
     card.appendChild(index);
@@ -88,24 +77,53 @@ function resetLibrary(parent) {
 
 //function to loop through array and create cards for each book
 
+let removeButton;
+
 function createLibrary(array) {
-    const library = document.querySelector('.library');
-
+        
     resetLibrary(library);
-
+    
     array.forEach((obj, index) => {
-        console.log(index);
+        //console.log(index);
         createCard(obj.title, obj.author, obj.pages, obj.read, index);
+    })
+
+    initializeRemoveButton();
+}
+
+//initialize remove button
+
+function initializeRemoveButton() {
+    
+    removeButton = document.querySelectorAll('.remove');
+    //console.log(removeButton);
+    removeButton.forEach(button => {
+        button.addEventListener('click', (event) => {
+            //console.log(event.originalTarget.parentElement.id);
+            removeBook(event.originalTarget.parentElement.id);
+        })
     })
 }
 
+//function to remove item from library
+
+function removeBook(index) {
+    myLibrary.splice(index, 1);
+    createLibrary(myLibrary);
+}
 
 
+//logic for modal window
 
+const modal = document.querySelector('.modal');
+const trigger = document.querySelector('.trigger');
+const closeButton = document.querySelector('.close-button');
+const submitButton = document.getElementById('submitButton');
 
-
-
-
+submitButton.addEventListener('click', handleUserInput);
+trigger.addEventListener('click', toggleModal);
+closeButton.addEventListener('click', toggleModal);
+window.addEventListener('click', windowOnClick);
 
 function toggleModal() {
     modal.classList.toggle('show-modal');
@@ -115,58 +133,22 @@ function windowOnClick(e) {
     if (e.target === modal) {
         toggleModal();
     }
-}
+} 
 
-submitButton.addEventListener('click', handleUserInput);
-trigger.addEventListener('click', toggleModal);
-closeButton.addEventListener('click', toggleModal);
-window.addEventListener('click', windowOnClick);
 
 //function to retrieve user data once 'submit' button is pressed
 
 function handleUserInput() {
-    
-    let titleValue = document.getElementById('title').value;
-    let authorValue = document.getElementById('author').value;
-    let pagesValue = document.getElementById('pages').value;
-    let readValue = document.getElementById('read').value;
+    const titleValue = document.getElementById('title').value;
+    const authorValue = document.getElementById('author').value;
+    const pagesValue = document.getElementById('pages').value;
+    const readValue = document.getElementById('read').value;
 
     addBookToLibrary(titleValue, authorValue, pagesValue, readValue);
-    //createCard(titleValue, authorValue, pagesValue, readValue);
     createLibrary(myLibrary);
     toggleModal();
 }
 
-//listen for remove button click
 
-
-// removeButton.forEach(button => {
-//     console.log('hit');
-// })
-
-//function to remove book and re-build library
-
-function removeBook(arrayIndex) {
-
-}
-
-//remove button event listener and capture button id (equal to array index);
-
-document.addEventListener("DOMContentLoaded", function() {
-    const removeButton = document.querySelectorAll('.remove');
-    console.log(removeButton);
-    removeButton.forEach(button => {
-        button.addEventListener('click', (event) => {
-            console.log(event.originalTarget.parentElement.id);
-        })
-    })
-})
-
-//listen for page load and load cards from array
-
-// window.addEventListener('load', (e) => {
-//     createLibrary(myLibrary);
-//     console.log('loaded');
-// });
 
 createLibrary(myLibrary);
